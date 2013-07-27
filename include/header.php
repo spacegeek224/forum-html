15be2e2b52cbc69c91a7caeff88791a1de73caf1
Ray Chung <rchung@kr-rchung1mm1.local>
2013-07-27 20:19:09 +0900

    init

---
+++ include/header.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: pearlc
+ * Date: 13. 7. 27
+ * Time: 오후 12:06
+ * To change this template use File | Settings | File Templates.
+ */
+
+header("Content-type: text/html; charset=utf-8");
+
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ko" xml:lang="ko">
+
+<head>
+    <title>GTA forum KR</title>
+    <meta http-equiv="Content-type" content="text/html; charset=utf8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap -->
+    <link href="/static/bs/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/static/css/docs.css" rel="stylesheet">
+    <link href="/static/css/common.css" rel="stylesheet">
+    <link href="/static/css/fonts.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+    <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+    <style>
+        .bs-footer {
+            padding-top: 40px;
+            padding-bottom: 30px;
+            margin-top: 100px;
+            text-align: center;
+            border-top: 1px solid #e5e5e5;
+        }
+    </style>
+</head>
+<body>
+
+
+<!-- Docs master nav -->
+<div class="navbar navbar-inverse navbar-fixed-top bs-docs-nav">
+    <div class="container">
+        <a href="../index.php" class="navbar-brand">GTA 한국 팬사이트</a>
+        <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <div class="nav-collapse collapse bs-navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="../getting-started/index.html">새소식</a></li>
+                <li class=""><a href="../getting-started/index.html">게임정보</a></li>
+                <li class=""><a href="../getting-started/index.html">자유게시판</a></li>
+                <li class=""><a href="../getting-started/index.html">동영상</a></li>
+                <li class=""><a href="../getting-started/index.html">스크린샷</a></li>
+                <li class=""><a href="../getting-started/index.html">GTA온라인</a></li>
+            </ul>
+
+            <ul class="nav navbar-nav pull-right">
+                <li><a href="http://getbootstrap.com/2.3.2/">관리자</a></li>
+            </ul>
+
+        </div>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,110 @@
+<?php
+require_once("include/header.php");
+?>
+
+<div class="bs-header">
+    <div class="container">
+        <h1>Getting started</h1>
+        <p>An overview of Bootstrap, how to download and use, basic templates and examples, and more.</p>
+        <div id="carbonads-container">
+            <div class="carbonad">
+<!--                <div id="azcarbon">-->
+<!--                </div>-->
+<!--                <script type="text/javascript">var z = document.createElement("script"); z.type = "text/javascript"; z.async = true; z.src = "http://engine.carbonads.com/z/32341/azcarbon_2_1_0_HORIZ"; var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(z, s);</script>-->
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="container bs-docs-container">
+<div class="row">
+    <div class="col-lg-3">
+        <div class="bs-sidebar">
+            <ul class="nav bs-sidenav">
+
+                <li>
+                    <a href="#download">Download Bootstrap</a>
+                    <ul class="nav">
+                        <li><a href="#download-options">Download options</a></li>
+                        <li><a href="#download-cdn">Bootstrap CDN</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#whats-included">What's included</a>
+                </li>
+                <li>
+                    <a href="#template">Basic template</a>
+                </li>
+                <li>
+                    <a href="#browsers">Browser support</a>
+                </li>
+                <li>
+                    <a href="#license-faqs">License FAQs</a>
+                </li>
+                <li>
+                    <a href="#customizing">Customizing Bootstrap</a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+    <div class="col-lg-9">
+        <div class="bs-docs-section">
+            <div class="page-header">
+                <h1>호랑이</h1>
+            </div>
+            <h3>Download compiled CSS and JS</h3>
+            <p class="lead">There are a few easy ways to quickly get started with Bootstrap, each one appealing to a different skill level and use case. Read through to see what suits your particular needs.</p>
+            <p><a class="btn btn-large btn-primary" href="assets/bootstrap.zip" onclick="_gaq.push(['_trackEvent', 'Getting started', 'Download', 'Download compiled']);">Download Bootstrap</a></p>
+            <hr>
+            <h3 id="download-options">More download options</h3>
+            <div class="bs-docs-dl-options">
+                <h4>
+                    <a href="https://github.com/twitter/bootstrap/zipball/master" onclick="_gaq.push(['_trackEvent', 'Getting started', 'Download', 'Download source']);">Download latest source code</a>
+                </h4>
+                <p>Get the original files for all CSS and JavaScript by downloading the latest version directly from GitHub.</p>
+                <h4>
+                    <a href="https://github.com/twitter/bootstrap/" onclick="_gaq.push(['_trackEvent', 'Getting started', 'Download', 'GitHub project']);">Clone or fork via GitHub</a>
+                </h4>
+                <p>Clone the entire project or fork your own version of Bootstrap to make it your own by visiting us on GitHub.</p>
+                <h4>
+                    Install with <a href="http://twitter.github.com/bower">Bower</a>
+                </h4>
+                <p>Install and manage the original files for all CSS and JavaScript, along with a local copy of the docs, using <a href="http://twitter.github.com/bower">Bower</a>.</p>
+                <div class="highlight"><pre><code class="bash"><span class="nv">$ </span>bower install bootstrap</code></pre></div>
+            </div>
+            <div class="bs-callout bs-callout-warning" id="callout-less-compilation">
+                <h4>LESS compilation</h4>
+                <p>If you download the original files, you need to compile Bootstrap's LESS files into usable CSS. To do that, Bootstrap only officially supports <a href="http://twitter.github.io/recess/">Recess</a>, Twitter's CSS hinter built on top of <a href="http://lesscss.org">less.js</a>.</p>
+            </div>
+
+        </div>
+
+        <div class="bs-docs-section" id="browsers">
+            <div class="page-header">
+                <h1>Browser compatibility</h1>
+            </div>
+            <p class="lead">Bootstrap is built to work best in the latest desktop and mobile browsers, meaning older and less advanced browsers might receive a less stylized, though fully functional, version of certain components.</p>
+
+            <h4>Supported browsers</h4>
+            <p>Specifically, we support the latest versions of the following:</p>
+            <ul>
+                <li>Chrome (Mac, Windows, iOS, and Android)</li>
+                <li>Safari (Mac and iOS only, as Windows has more or less been discontinued)</li>
+                <li>Firefox (Mac, Windows)</li>
+                <li>Internet Explorer</li>
+                <li>Opera (Mac, Windows)</li>
+            </ul>
+            <p><strong>Internet Explorer 8 and 9</strong> are also supported. However, please be aware that many CSS3 properties—e.g., rounded corners and shadows—are not supported by IE8. Unofficially, Bootstrap should look and behave well enough in Chromium for Linux and Internet Explorer 7, though they are not officially supported.</p>
+
+            <h4>IE Compatibility modes</h4>
+            <p>Bootstrap is not supported in the old Internet Explorer compatibility modes. To be sure you're using the latest rendering mode for IE, consider including <code>&lt;meta http-equiv="X-UA-Compatible" content="IE=edge"&gt;</code> in your pages. See <a href="http://stackoverflow.com/questions/6771258/whats-the-difference-if-meta-http-equiv-x-ua-compatible-content-ie-edge">this StackOverflow question</a> for more information.</p>
+        </div>
+
+    </div>
+</div>
+</div>
+<?php
+require_once("include/footer.php");
+?>
