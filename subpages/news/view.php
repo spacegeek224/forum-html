16f907bc01529da0b8af14a1f8d48da433df88e6
Ray Chung <rchung@kr-rchung1mm1.local>
2013-07-28 20:05:23 +0900

    adds news subview

---
+++ subpages/news/view.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * User: rchung
+ * Date: 13. 7. 28.
+ * Time: 오후 7:54
+ * To change this template use File | Settings | File Templates.
+ */
+
+chdir("../../");
+require_once("include/header.php");
+?>
+
+    <div class="container bs-docs-container">
+        <div class="row">
+            <div class="col-lg-3">
+                <?php
+                include ("include/sidebar.php");
+                ?>
+            </div>
+            <div class="col-lg-9">
+                <div class="bs-docs-section">
+                    <div class="page-header">
+                        <h4>GTA5 게임 플레이 영상 최초 공개</h4>
+                    </div>
+                    <p></p>
+            </div>
+        </div>
+    </div>
+    </div>
+
+
+
+
+<?php
+require_once("include/footer.php");
